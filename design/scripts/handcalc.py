772d616eb8fabd80494fb91811402b9a0165f291
NohealthyBBQ <zexil@andrew.cmu.edu>
2022-07-28 11:06:42 -0600

    nmos and pmos calc

--- design/scripts/handcalc.py
+++ design/scripts/handcalc.py
@@ -1,36 +1,13 @@
-##NMOS 1.8V lvt
-#epsrox = 3.9
-#E0 = 8.85e-14 #F/cm
-#toxm = 4.148e-7 #cm
-#
-#cox = epsrox * E0 / toxm #F/cm^2
-#
-#print("cox = "+str(cox))
-#u0 = 3.155047379e-02 * 1e4 #cm^2/V/s
-#
-#uCox = u0 * cox
-#print("uCox = "+str(uCox * 1e6)+" uA/V^2")
-#
-##PMOS 1.8V lvt
-#epsrox = 3.9
-#E0 = 8.85e-14 #F/cm
-#toxm = 4.148e-7 #cm
-#
-#cox = epsrox * E0 / toxm #F/cm^2
-#
-#print("cox = "+str(cox))
-#u0 = 3.155047379e-02 * 1e4 #cm^2/V/s
-#
-#uCox = u0 * cox
-#print("uCox = "+str(uCox * 1e6)+" uA/V^2")
+#!/usr/bin/env python
 
 def calculate_uCox(toxm, u0, epsrox=3.9, E0=8.85e-14, ):
     cox = epsrox * E0 / toxm #F/cm^2
     uCox = u0 * cox
     print("cox = "+str(cox) + " F/cm^2")
     print("uCox = "+str(uCox * 1e6)+" uA/V^2")
-
+    return uCox
 
 
 if __name__ == "__main__":
-    calculate_uCox(toxm=4.148e-7,u0=3.155e-2*1e4)    
+    nmos_1p8_lvt_uCox  = calculate_uCox(toxm=4.148e-7,u0=3.155e-2*1e4)    
+    pmos_1p8_lvt_uCox  = calculate_uCox(toxm=4.23e-7,u0=0.0025*1e4)    
