1f0e7aeb8c936e85a152538e0485e10a66a11655
NohealthyBBQ <zexil@andrew.cmu.edu>
2022-07-28 10:57:22 -0600

    functions

--- design/scripts/handcalc.py
+++ design/scripts/handcalc.py
@@ -1,11 +1,36 @@
-epsrox = 3.9
-E0 = 8.85e-14 #F/cm
-toxm = 4.148e-7 #cm
+##NMOS 1.8V lvt
+#epsrox = 3.9
+#E0 = 8.85e-14 #F/cm
+#toxm = 4.148e-7 #cm
+#
+#cox = epsrox * E0 / toxm #F/cm^2
+#
+#print("cox = "+str(cox))
+#u0 = 3.155047379e-02 * 1e4 #cm^2/V/s
+#
+#uCox = u0 * cox
+#print("uCox = "+str(uCox * 1e6)+" uA/V^2")
+#
+##PMOS 1.8V lvt
+#epsrox = 3.9
+#E0 = 8.85e-14 #F/cm
+#toxm = 4.148e-7 #cm
+#
+#cox = epsrox * E0 / toxm #F/cm^2
+#
+#print("cox = "+str(cox))
+#u0 = 3.155047379e-02 * 1e4 #cm^2/V/s
+#
+#uCox = u0 * cox
+#print("uCox = "+str(uCox * 1e6)+" uA/V^2")
 
-cox = epsrox * E0 / toxm #F/cm^2
+def calculate_uCox(toxm, u0, epsrox=3.9, E0=8.85e-14, ):
+    cox = epsrox * E0 / toxm #F/cm^2
+    uCox = u0 * cox
+    print("cox = "+str(cox) + " F/cm^2")
+    print("uCox = "+str(uCox * 1e6)+" uA/V^2")
 
-print("cox = "+str(cox))
-u0 = 3.155047379e-02 * 1e4 #cm^2/V/s
 
-uCox = u0 * cox
-print("uCox = "+str(uCox * 1e6)+" uA/V^2")
+
+if __name__ == "__main__":
+    calculate_uCox(toxm=4.148e-7,u0=3.155e-2*1e4)    
