edebe285c957e762251fcfeb4e32f68e61101703
NohealthyBBQ <zexil@andrew.cmu.edu>
2022-09-07 16:58:34 -0600

    tcl files to streamline lvs

---
+++ design/opamp/layout/netgen.sh
@@ -0,0 +1,1 @@
+netgen -batch lvs "opamp_realcomp3_usefinger.spice opamp_realcomp3_usefinger" "lvs/opamp_realcomp3_usefinger.spice opamp_realcomp3_usefinger" /pdk/open_pdks/install/share/pdk/sky130A/libs.tech/netgen/setup.tcl compare_out
